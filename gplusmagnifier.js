80ba5b0f4c4cbbb9c6e9896a94e0a088dada40bc
Roultabie <daniel.douat@aelys-info.fr>
2013-10-04 16:12:32 +0200

    separated functions

--- gplusmagnifier.js
+++ gplusmagnifier.js
@@ -3,24 +3,9 @@
 // @namespace   aelys-info.fr
 // @include     https://plus.google.com/*
 // @include     http://plus.google.com/*
-// @version     1
+// @version     2
 // ==/UserScript==
 (function () {
-    function injectCSS(cssdata)
-    {
-        head = document.getElementsByTagName("head")[0];
-        style = document.createElement("style");
-        style.setAttribute("type", 'text/css');
-        style.innerHTML = cssdata;
-        head.appendChild(style);
-    }
-
-    var posy = 102;
-
-    function gmRemoveCommunities()
-    {
-        injectCSS('.Om { display: none !important;}');
-    }
 
     function cursorPosition(event)
     {
@@ -45,7 +30,6 @@
         window.onscroll = testScroll;
         document.onmousemove = cursorPosition;
         setTimeout(testScroll, 500); 
-        setTimeout(gmRemoveCommunities, 500);
     }
     catch (e)
     {
